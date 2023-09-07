fbc179a3bb836606d64236eb05a620b71bafac8f
Menarady02 <menarady023@gmail.com>
2023-09-07 16:46:05 -0700

    M

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-printf("char:%zu short:%zu int:%zu long:%zu long long:%zu float:%zu double:%zu long double:%zu\n",sizeof(char),sizeof(short),sizeof(int),sizeof(long),sizeof(long long),sizeof(float),sizeof(double),sizeof(long double));
+printf("char:%zu short:%zu int:%zu long:%zu long long:%zu float:%zu double:%zu long double:%zu\n", sizeof(char), sizeof(short), sizeof(int), sizeof(long), sizeof(long long), sizeof(float), sizeof(double), sizeof(long double));
 return 0;
 }
