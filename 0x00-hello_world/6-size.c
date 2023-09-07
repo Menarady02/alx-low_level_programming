54069a2464206b25224165ccb85a53902575d31f
Menarady02 <menarady023@gmail.com>
2023-09-07 16:43:16 -0700

    M

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,6 @@
 
 int main(void)
 {
-printf("size of a char: %u byte(s)\n", sizeof(char));
-printf("size of an int: %u byte(s)\n", sizeof(int));
-printf("size of a long int: %u byte(s)\n", sizeof(long int));
-printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
-printf("size of a float: %u byte(s)\n", sizeof(float));
-return (0);
+printf("char:%zu short:%zu int:%zu long:%zu long long:%zu float:%zu double:%zu long double:%zu\n",sizeof(char),sizeof(short),sizeof(int),sizeof(long),sizeof(long long),sizeof(float),sizeof(double),sizeof(long double));
+return 0;
 }
